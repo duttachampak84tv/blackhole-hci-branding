867694047f865f74696b8a1685f476e11cf483f3
duttachampak84tv <duttachampak84tv@gmail.com>
2026-03-23 12:28:38 +0530

    Create deployment script deploy-branding.sh

---
+++ deploy-branding.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Deployment script for BlackHole HCI customizations
+
+# Set variables
+REPO_NAME="blackhole-hci-branding"
+
+# Display the current date and time
+echo "Deployment started on $(date)"
+
+# Add deployment commands here
+
+# End of script
+
+echo "Deployment completed"
